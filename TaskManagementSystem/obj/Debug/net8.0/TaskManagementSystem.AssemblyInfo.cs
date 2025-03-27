8b9dfdd1131225d3652eaf2f0871b7c91afc8c21
Olaposi <seunposifunmi@gmail.com>
2025-03-27 14:57:14 +0100

    messahe

--- TaskManagementSystem/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
+++ TaskManagementSystem/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f7ceb1610e96e235d569cb69598728ead7df57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3e667fd430830ffd1704a09512a317f7a98a6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
